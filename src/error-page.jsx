c3a7a30c7d317c414171e1f3a35c6632e5f2f1de
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-05-20 20:38:25 +0600

    404 page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,19 @@
 import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import img1 from "../public/images/404.jpg";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" className="">
+      <img src={img1} alt="" />
+      <p>{/* <i>{error.statusText || error.message}</i> */}</p>
+      <button className="btn btn-accent">
+        <Link to="/">Go to Home</Link>
+      </button>
     </div>
   );
 }
