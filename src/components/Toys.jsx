136a780f6339a6685b598ebe94cca07578118471
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-11 20:01:48 +0600

    Single toy details in Modal

--- src/components/Toys.jsx
+++ src/components/Toys.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Toys = ({ toy }) => {
   return (
@@ -13,19 +12,24 @@ const Toys = ({ toy }) => {
           <p>Price: {toy.Price}</p>
           <p>Rating: {toy.Rating}</p>
           {/* The button to open modal */}
-          <label htmlFor="my_modal_6" className="btn">
+          <label htmlFor={toy._id} className="btn">
             open modal
           </label>
 
           {/* Put this part before </body> tag */}
-          <input type="checkbox" id="my_modal_6" className="modal-toggle" />
+          <input type="checkbox" id={toy._id} className="modal-toggle" />
           <div className="modal">
             <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
+              <h3 className="font-bold text-lg">Toy Name: {toy.Name}</h3>
               <img className="w-1/2" src={toy.Picture} alt="toy" />
-              <p className="py-4">This modal works with a hidden checkbox!</p>
+              <p className="py-4">Seller Name: {toy.Seller_Name}</p>
+              <p className="py-4">Seller Email: {toy.Seller_Email}</p>
+              <p className="py-4">Price: {toy.Price}</p>
+              <p className="py-4">Rating: {toy.Rating}</p>
+              <p className="py-4">Quantity: {toy.Quantity}</p>
+              <p className="py-4">Decription: {toy.Description}</p>
               <div className="modal-action">
-                <label htmlFor="my_modal_6" className="btn">
+                <label htmlFor={toy._id} className="btn">
                   Close!
                 </label>
               </div>
