740a12c9957d2e277f2570ff7ba03e3ffc207a01
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-12 07:25:13 +0600

    added npm i react-hook-form

--- src/components/AddAToy.jsx
+++ src/components/AddAToy.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 import { Helmet } from "react-helmet-async";
 
 const AddAToy = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <Helmet>
         <title>Toy Story | Add a Toy </title>
       </Helmet>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </div>
   );
 };
