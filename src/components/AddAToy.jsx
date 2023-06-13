40e8b918ff55178ce339451cc949cb4e02d4e8ca
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-13 11:46:46 +0600

    post to Mongodb done

--- src/components/AddAToy.jsx
+++ src/components/AddAToy.jsx
@@ -1,14 +1,30 @@
 import { useForm } from "react-hook-form";
 import { Helmet } from "react-helmet-async";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const AddAToy = () => {
+  const { user } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    fetch("http://localhost:5000/postToy", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+
+    console.log(data);
+  };
 
   return (
     <div>
@@ -23,17 +39,58 @@ const AddAToy = () => {
         {/* register your input into the hook by invoking the "register" function */}
         <input
           className="border rounded p-2 my-2"
-          defaultValue="test"
-          {...register("example")}
+          placeholder="URL Picture "
+          {...register("URL")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Name "
+          {...register("name")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Seller Name "
+          {...register("sellerName")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Seller Email "
+          value={user.email}
+          {...register("sellerEmail")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Sub Category "
+          {...register("subCategory")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Price "
+          {...register("price")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Rating "
+          {...register("rating")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Available Quantity "
+          {...register("quantity")}
+        />
+        <input
+          className="border rounded p-2 my-2"
+          placeholder="Detail Description "
+          {...register("description")}
         />
 
         {/* include validation with required or other standard HTML validation rules */}
-        <input
+        {/* <input
           className="border rounded p-2 my-2"
           {...register("exampleRequired", { required: true })}
-        />
+        /> */}
         {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+        {/* {errors.exampleRequired && <span>This field is required</span>} */}
 
         <input
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
