8307afce326947915f1b95272d9303cf73e43dcf
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-14 07:50:46 +0600

    temp

--- src/components/AllToys.jsx
+++ src/components/AllToys.jsx
@@ -20,7 +20,49 @@ const AllToys = () => {
 
       <div>
         {toys?.map((toy) => (
-          <Toy key={toy._id} toy={toy}></Toy>
+          <div className="overflow-x-auto">
+            <thead>
+              <tr>
+                <th>Seller</th>
+                <th>Toy Name</th>
+                <th>Sub-Category</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>View Details</th>
+              </tr>
+            </thead>
+            <table className="table">
+              {/* head */}
+              {/* <Toy key={toy._id} toy={toy}></Toy> */}
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img src={toy.URL} alt="" />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{toy.sellerName}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{toy.name}</td>
+                  <td>{toy.subCategory}</td>
+                  <td>{toy.price}</td>
+                  <td>{toy.quantity}</td>
+                  <td>{toy.description}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         ))}
       </div>
     </div>
--- src/components/Toy.jsx
+++ src/components/Toy.jsx
@@ -14,32 +14,11 @@ const Toy = ({ toy }) => {
   } = toy || {};
   return (
     <div>
-      {/* <div className="card lg:card-side bg-base-100 shadow-xl">
-        <figure>
-          <img src={URL} alt="Album" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{sellerName}</p>
-          <p>{sellerEmail}</p>
-          <p>{subCategory}</p>
-          <p>{price}</p>
-          <p>{rating}</p>
-          <p>{quantity}</p>
-          <p>{description}</p>
-        </div>
-      </div> */}
-
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
               <th>Seller</th>
               <th>Toy Name</th>
               <th>Sub-Category</th>
@@ -60,7 +39,7 @@ const Toy = ({ toy }) => {
                 <div className="flex items-center space-x-3">
                   <div className="avatar">
                     <div className="mask mask-squircle w-12 h-12">
-                      <img src={URL} alt="Avatar Tailwind CSS Component" />
+                      <img src={URL} alt="" />
                     </div>
                   </div>
                   <div>
@@ -68,131 +47,13 @@ const Toy = ({ toy }) => {
                   </div>
                 </div>
               </td>
-              <td>
-                {name}
-                <br />
-              </td>
+              <td>{name}</td>
               <td>{subCategory}</td>
               <td>{price}</td>
               <td>{quantity}</td>
               <td>{description}</td>
             </tr>
-            {/* row 2 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-3@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Tax Accountant
-                </span>
-              </td>
-              <td>Red</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-4@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Rowe-Schoen
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Office Assistant I
-                </span>
-              </td>
-              <td>Crimson</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 4 */}
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-5@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Wyman-Ledner
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Community Outreach Specialist
-                </span>
-              </td>
-              <td>Indigo</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
           </tbody>
-          {/* foot */}
-          <tfoot>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </tfoot>
         </table>
       </div>
     </div>
