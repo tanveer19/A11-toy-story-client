659b094636ef8ad8679a1721a1b653f26964bb96
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-06-25 21:37:50 +0600

    Update ShopByCategory.jsx

--- src/components/ShopByCategory.jsx
+++ src/components/ShopByCategory.jsx
@@ -52,7 +52,24 @@ const ShopByCategory = () => {
       </div>
       <Tabs>
         <TabList>
-          <Tab>sports car</Tab>
+          <Tab>
+            <div className="dropdown">
+              <label tabIndex={0} className="btn m-1">
+                Sports car
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
+            </div>
+          </Tab>
           <Tab>truck</Tab>
           <Tab>mini police car</Tab>
         </TabList>
