2db3eea0c71f7d66fd164a2a7431654dca9ea274
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-07-11 00:12:12 +0600

    static data in modal

--- src/components/MyToys.jsx
+++ src/components/MyToys.jsx
@@ -117,24 +117,6 @@ const MyToys = () => {
                       Edit
                     </label>
 
-                    {/* Put this part before </body> tag */}
-                    <input
-                      type="checkbox"
-                      id="my_modal_7"
-                      className="modal-toggle"
-                    />
-                    <div className="modal">
-                      <div className="modal-box">
-                        <h3 className="text-lg font-bold">Hello!</h3>
-                        <p className="py-4">
-                          This modal works with a hidden checkbox!
-                        </p>
-                      </div>
-                      <label className="modal-backdrop" htmlFor="my_modal_7">
-                        Close
-                      </label>
-                    </div>
-
                     <UpdateToyModal
                       show={modalShow}
                       onHide={() => setModalShow(false)}
--- src/components/UpdateToyModal.jsx
+++ src/components/UpdateToyModal.jsx
@@ -9,9 +9,24 @@ const UpdateToyModal = (props) => {
     formState: { errors },
   } = useForm();
 
-  const { handleJobUpdate } = props;
-  //   console.log(handleJobUpdate);
-  return <div></div>;
+  const { handleToyUpdate } = props;
+  //   console.log(handleToyUpdate);
+  return (
+    <div>
+      {/* Put this part before </body> tag */}
+
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">{props.toy.sellerName}</h3>
+          <p className="py-4">{props.toy.name}</p>
+        </div>
+        <label className="modal-backdrop" htmlFor="my_modal_7">
+          Close
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateToyModal;
