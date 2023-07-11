57a1f36d26ff9fa806a3ab4a197658f1915b0f9e
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-07-11 13:12:15 +0600

    t

--- src/components/UpdateToyModal.jsx
+++ src/components/UpdateToyModal.jsx
@@ -5,38 +5,82 @@ const UpdateToyModal = (props) => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
-  const { toy, handleToyUpdate, onHide } = props;
+  const { handleToyUpdate } = props;
 
   const onSubmit = (data) => {
     handleToyUpdate(data);
   };
+
   return (
     <div {...props}>
       <input type="checkbox" id="my_modal_7" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <h3 className="text-lg font-bold"> Update Toy</h3>
-
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <h3 className="text-lg font-bold">Update </h3>
+          <form
+            className="container text-center"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {errors.exampleRequired && <span>This field is required</span>}
             <input
-              className="text-input"
-              {...register("title")}
+              className="form-input"
+              {...register("sellerName")}
               placeholder="Title"
-              defaultValue={toy?.name}
+              defaultValue={props?.toy?.sellerName}
             />
-            <input type="hidden" {...register("_id")} defaultValue={toy?._id} />
-            <button type="submit" className="btn">
-              Update
-            </button>
+            <input
+              className="form-input d-none"
+              {...register("_id")}
+              value={props?.toy?._id}
+            />
+
+            <input
+              className="form-input"
+              {...register("name", { required: true })}
+              placeholder="Salary"
+              defaultValue={props?.toy?.name}
+            />
+            <input
+              className="form-input"
+              {...register("price", { required: true })}
+              placeholder="Vacancy"
+              defaultValue={props?.toy?.price}
+            />
+            <select
+              className="form-select"
+              {...register("subCategory")}
+              defaultValue={props?.toy?.subCategory}
+            >
+              <option value="Engineering">Engineering</option>
+              <option value="Editor">Editor</option>
+              <option value="Writer">Writer</option>
+              <option value="Developer">Developer</option>
+            </select>
+            <select className="form-select" {...register("status")}>
+              <option value="remote">Remote</option>
+              <option value="offline">Offline</option>
+            </select>
+            <input
+              className="form-input"
+              {...register("URL")}
+              placeholder="URL link"
+              type="url"
+              defaultValue={props?.toy?.URL}
+            />
+            <input
+              className="form-input"
+              {...register("rating")}
+              placeholder="Deadline"
+              type="date"
+              defaultValue={props?.toy?.rating}
+            />
+            <input className="btn" value="Update Job" type="submit" />
           </form>
         </div>
-        <label className="modal-backdrop" htmlFor="my_modal_7" onClick={onHide}>
-          Close
-        </label>
+        <label className="modal-backdrop" htmlFor="my_modal_7" />
       </div>
     </div>
   );
