b053b283d9f1f1fc1f22144d1e5431a739be0a97
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-09-23 17:53:48 +0600

    google-site-verification

--- src/components/GridSections.jsx
+++ src/components/GridSections.jsx
@@ -6,40 +6,29 @@ const GridSections = () => {
       <h4 className="text-5xl text-center my-4"> Our Cars </h4>
       <div class="grid grid-cols-3 gap-4 w-1/2 mx-auto">
         <div class="flex justify-center items-center">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScy7n9TVq1QCC5ijgtoh-xRLrghQNZEMZGxA&usqp=CAU"
-            alt=""
-          />
+          <img src="https://i.ibb.co/grzGwgT/Bugatti.png" alt="car" />
+        </div>
+        <div class="flex justify-center items-center">
+          <img src="https://i.ibb.co/zbNS1CV/Ferrari.png" alt="car" />
+        </div>
+        <div class="flex justify-center items-center">
+          <img src="https://i.ibb.co/5ML8BXv/Lamborghini2.png" alt="car" />
+        </div>
+        <div class="col-span-2 flex justify-center items-center">
+          <img src="/images/car5.png" alt="car" />
+        </div>
+        <div class="flex justify-center items-center">
+          <img src="https://i.ibb.co/JxS1qqQ/nissan.png" alt="car" />
         </div>
         <div class="flex justify-center items-center">
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ8XqOV8-AQ0GAp2PY5p6Iz1Vj7Bc4U7vEug&usqp=CAU"
-            alt=""
-          />
-        </div>
-        <div class="flex justify-center items-center">
-          <img
-            src="https://images.unsplash.com/photo-1595641416278-c4469e0a22d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHRveSUyMGNhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-            alt=""
+            src="https://i.ibb.co/0DvCCf6/Truck.png
+"
+            alt="car"
           />
         </div>
         <div class="col-span-2 flex justify-center items-center">
-          <img src="/images/car5.png" alt="" />
-        </div>
-        <div class="flex justify-center items-center">
-          <img
-            src="https://images.unsplash.com/photo-1616850508698-8cb0c576d5ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fHRveSUyMGNhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-            alt=""
-          />
-        </div>
-        <div class="flex justify-center items-center">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQovS-T2atHiYAhu5XNSHltbfljilzri4DeCw&usqp=CAU"
-            alt=""
-          />
-        </div>
-        <div class="col-span-2 flex justify-center items-center">
-          <img src="/images/car1.jpg" alt="" />
+          <img src="/images/car1.jpg" alt="car" />
         </div>
       </div>
     </div>
